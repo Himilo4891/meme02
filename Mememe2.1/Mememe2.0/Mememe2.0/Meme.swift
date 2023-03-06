89458262a26093c83f6cc04e7b4728506dab6f11
Himilo4891 <118472464+Himilo4891@users.noreply.github.com>
2023-03-06 10:16:51 +0300

    Add files via upload

---
+++ Mememe2.1/Mememe2.0/Mememe2.0/Meme.swift
@@ -0,0 +1,19 @@
+
+import Foundation
+import UIKit
+
+
+struct Meme {
+    
+    let name: String
+    let evilScheme: String
+    let imageName: String
+    class Meme {
+        private var topTextField: String?
+        private var bottomTextField: String?
+        private var originalImage: UIImage?
+        private var memedImage: UIImage?
+        
+        
+    }
+}
